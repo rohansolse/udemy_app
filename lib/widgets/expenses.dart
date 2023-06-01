910f33a2f51bbd881bc658ec3c161f60d382dc57
yashrkhandelwal <40458275+rohansolse@users.noreply.github.com>
2023-06-01 16:09:23 +0530

    New Expense Added

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:udemy_app/widgets/expenses_list/expenses_list.dart';
 import 'package:udemy_app/models/expense.dart';
+import 'package:udemy_app/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -24,6 +25,12 @@ class _ExpensesState extends State<Expenses> {
       category: Category.leisure,
     ),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,14 +38,13 @@ class _ExpensesState extends State<Expenses> {
         title: const Text('Flutter Expense Tracker'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
             icon: const Icon(Icons.add),
           ),
         ],
       ),
       body: Column(
         children: [
-          const Text('The Chart'),
           Expanded(
             child: ExpensesList(expenses: _registeredExpenses),
           )
