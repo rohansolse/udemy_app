1bcd3ab61a1949a0972f140048dce73b6a865c20
Rohan.Solse <rohan.solse@baptisthealth.net>
2023-07-11 13:41:00 +0530

    120 done

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -43,13 +43,38 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 5),
+        content: const Text('Expense Deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget contentToShow = const Center(
+      child: Text("No expenses added yet. Add something to show"),
+    );
+    if (_registeredExpenses.isNotEmpty) {
+      contentToShow = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
@@ -61,14 +86,7 @@ class _ExpensesState extends State<Expenses> {
         ],
       ),
       body: Column(
-        children: [
-          Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: removeExpense,
-            ),
-          ),
-        ],
+        children: [Expanded(child: contentToShow)],
       ),
     );
   }
