0d127b4b9d41e2304ad08298c795e8dd4475c02d
Rohan.Solse <rohan.solse@baptisthealth.net>
2023-07-17 12:34:11 +0530

    theme & chart model done

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,35 @@ class MyApp extends StatelessWidget {
     seedColor: const Color.fromARGB(255, 96, 59, 181),
   );
 
+  var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 5, 99, 125),
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        // appBarTheme: const AppBarTheme().copyWith(
+        //   backgroundColor: kDarkColorScheme.onPrimaryContainer,
+        //   foregroundColor: kDarkColorScheme.primaryContainer,
+        // ),
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
