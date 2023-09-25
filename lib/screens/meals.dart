5ac6b93f211829d64d63075c5c2ba0bb6b3a0a79
Rohan.Solse <rohan.solse@baptisthealth.net>
2023-09-25 17:23:45 +0530

    160 done

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:udemy_app/models/meal.dart';
+import 'package:udemy_app/widget/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -11,10 +12,7 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (ctx, index) => Text(
-        meals[index].title,
-        style: const TextStyle(color: Colors.white),
-      ),
+      itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
     );
 
     if (meals.isEmpty) {
