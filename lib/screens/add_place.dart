616634f38f3c3a5d5a2eecba8fb54c6fc69f9965
Rohan.Solse <rohan.solse@baptisthealth.net>
2023-11-07 16:50:37 +0530

    240 done

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:udemy_app/providers/user_places.dart';
+import 'package:udemy_app/widgets/image_input.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -46,6 +47,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 style: TextStyle(
                     color: Theme.of(context).colorScheme.onBackground),
               ),
+              const SizedBox(height: 10),
+              const ImageInput(),
               const SizedBox(height: 16),
               ElevatedButton.icon(
                 onPressed: _savePlace,
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  void _takePicture() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: 250,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+        ),
+      ),
+      child: TextButton.icon(
+        icon: const Icon(Icons.camera),
+        label: const Text('Take Picture'),
+        onPressed: _takePicture,
+      ),
+    );
+  }
+}
