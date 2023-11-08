70cca3d40cc5243544f8b000e253e8a0e698dffb
Rohan.Solse <rohan.solse@baptisthealth.net>
2023-11-08 16:02:34 +0530

    working wakescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+// Import necessary packages
 import 'package:flutter/material.dart';
+import 'package:wakelock/wakelock.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,27 +11,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo First',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return const MaterialApp(
+      home: MyRecordingScreen(),
+    );
+  }
+}
+
+class MyRecordingScreen extends StatefulWidget {
+  const MyRecordingScreen({super.key});
+
+  @override
+  _MyRecordingScreenState createState() => _MyRecordingScreenState();
+}
+
+class _MyRecordingScreenState extends State<MyRecordingScreen> {
+  bool isRecording = false;
+
+  // Start recording and enable screen wakelock
+  void startRecording() {
+    setState(() {
+      isRecording = true;
+    });
+    Wakelock.enable();
+    // Start your recording logic here
+  }
+
+  // Stop recording and disable screen wakelock
+  void stopRecording() {
+    setState(() {
+      isRecording = false;
+    });
+    Wakelock.disable();
+    // Stop your recording logic here
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recording Screen'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App Fresh'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              isRecording ? 'Recording...' : 'Not Recording',
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: isRecording ? stopRecording : startRecording,
+              child: Text(isRecording ? 'Stop Recording' : 'Start Recording'),
+            ),
+          ],
         ),
       ),
     );
